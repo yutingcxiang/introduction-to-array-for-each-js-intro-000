7fc6754650b765437d9322766844c0769a20f480
Christine Xiang <yutingcxiang@gmail.com>
2018-06-18 21:23:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ function doToElementsInArray(array, callback) {
 }
 
 // Add your changeCompletely() function here:
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+}
+ 
+var animals = ["dog", "cat", "squirrel"];
+ 
+doToElementsInArray(animals, changeCompletely);
+ 
+// log out animals -- pretty cool, right?
+console.log(animals);
